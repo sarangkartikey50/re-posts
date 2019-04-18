6ae6dc92292161a1c9bc578caf4a52810112e9d9
sarang kartikey <sarangkartikey50@gmail.com>
2019-04-18 23:04:39 +0530

    Updates

---
+++ precache-manifest.681621d15c8d8a49ba4920d7d66d0876.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3352ed35321259cbd6ba84be4b844dc8",
+    "url": "/re-posts/static/media/logo.3352ed35.svg"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/re-posts/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/re-posts/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/re-posts/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "310aa2e248ab0822bc76",
+    "url": "/re-posts/static/js/runtime~main.51e13d48.js"
+  },
+  {
+    "revision": "8887551282dbd12436a1",
+    "url": "/re-posts/static/js/main.21102178.chunk.js"
+  },
+  {
+    "revision": "8fc8d29d38cbead52809",
+    "url": "/re-posts/static/js/2.36fb62d6.chunk.js"
+  },
+  {
+    "revision": "8887551282dbd12436a1",
+    "url": "/re-posts/static/css/main.fe254fbc.chunk.css"
+  },
+  {
+    "revision": "6fce96dca56da8dabde5384538dc9b4b",
+    "url": "/re-posts/index.html"
+  }
+];
