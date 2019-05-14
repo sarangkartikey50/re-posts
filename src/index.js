1676cfa705df4fdbd567e8b6d397c4e8c50c5f80
Sarang Kartikey <skartikey@yodlee.com>
2019-05-14 16:57:34 +0530

    react-snap changes added

--- src/index.js
+++ src/index.js
@@ -6,10 +6,21 @@ import './styles/index.css'
 import store from './store'
 import { Provider } from 'react-redux'
 
-ReactDOM.render(
-<Provider store={store}>
-    <App />
-</Provider>, document.getElementById('root'));
+const rootElement = document.getElementById('root')
+
+if(rootElement.hasChildNodes()){
+    ReactDOM.hydrate(
+        <Provider store={store}>
+            <App />
+        </Provider>, rootElement
+    )
+} else {
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>, rootElement
+    )
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
